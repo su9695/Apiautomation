1f9698a3c08273622d5eb9a0d8d4169a3fbda7bb
ww <978499129@qq.com>
2020-06-07 18:48:38 +0800

    log日志完善

--- test_case/test_one.py
+++ test_case/test_one.py
@@ -1,15 +1,17 @@
 # coding:utf-8
 import requests
 import pytest
+import json
 import sys
 import os
 import configparser
 sys.path.append('../')
-from util.handle_json import handle_jsonData
-from util.handle_init import handle_ini
-from base.base_request import baseRequest
+from Apiautomation.util.handle_json import handle_jsonData
+from Apiautomation.util.handle_init import handle_ini
+from Apiautomation.base.base_request import baseRequest
+from Apiautomation.util.log import run_log as logger
 
-baseFileName = '/ApiTestProject/Apiautomation/test_data/getrequest.json'
+baseFileName = '/ApiTestProject/Apiautomation/test_data/getRequest.json'
 
 
 class TestOne():
@@ -19,10 +21,10 @@ class TestOne():
         method = handle_jsonData.getJson_value('method',baseFileName)
         header = handle_jsonData.getJson_value('headers', baseFileName)
         paramsData = handle_jsonData.getJson_value('params', baseFileName)
-        #result = requests.get(url=url, headers = header, params=paramsData).json()
         responseData = baseRequest.run_main(method,url,paramsData,"",header)
-        print(responseData)
-
+        assert responseData['msg'] == "成功"
+        assert responseData['data'][0]['word'] == "python"
+        logger.error("测试日志模块")
 
 if __name__ == "__main__":
     pytest.main(["-s", "test_one.py"])
---
+++ util/log.py
@@ -0,0 +1,55 @@
+# coding=utf-8
+import os
+import sys
+import logbook
+from logbook import Logger,StreamHandler,FileHandler,TimedRotatingFileHandler
+from logbook.more import ColorizedStderrHandler
+
+def log_type(record,handler):
+    log = "[{date}] [{level}] [{filename}] [{func_name}] [{lineno}] {msg}".format(
+        date = record.time,                              # 日志时间
+        level = record.level_name,                       # 日志等级
+        filename = os.path.split(record.filename)[-1],   # 文件名
+        func_name = record.func_name,                    # 函数名
+        lineno = record.lineno,                          # 行号
+        msg = record.message                             # 日志内容
+    )
+    return log
+
+# 日志存放路径
+LOG_DIR = '/ApiTestProject/Apiautomation/log'
+print(LOG_DIR)
+if not os.path.exists(LOG_DIR):
+    os.makedirs(LOG_DIR)
+# 日志打印到屏幕
+log_std = ColorizedStderrHandler(bubble=True)
+log_std.formatter = log_type
+# 日志打印到文件
+log_file = TimedRotatingFileHandler(
+    os.path.join(LOG_DIR, '%s.log' % 'log'),date_format='%Y-%m-%d', bubble=True, encoding='utf-8')
+log_file.formatter = log_type
+
+# 脚本日志
+run_log = Logger("global_log")
+def init_logger():
+    logbook.set_datetime_format("local")
+    run_log.handlers = []
+    run_log.handlers.append(log_file)
+    run_log.handlers.append(log_std)
+    return ""
+
+'''
+日志等级：
+critical    严重错误，会导致程序退出
+error	可控范围内的错误
+warning	警告信息
+notice	大多情况下希望看到的记录
+info	大多情况不希望看到的记录
+debug	调试程序时详细输出的记录
+'''
+# 实例化，默认调用
+logger = init_logger()
+
+
+# if __name__ == "__main__":
+#     run_log.info("测试日志模块")
