169789d9b0e6c76e6362432c3d8540a34a020857
ww <ww23928@ly.com>
2020-07-16 09:03:35 +0800

    1111

--- util/handle_checkresult.py
+++ util/handle_checkresult.py
@@ -8,17 +8,32 @@ sys.path.append('../')
 
 class HandleCheckResult:
 
-    def check_result(self, apiResponseData,case_data):
+    def check_result(self, apiResponseData, case_data):
         """
         校验测试结果
         :return:
         """
         try:
-            
-            pass
+            comparator = case_data['validate'][0]['comparator']
+            check = case_data['validate'][0]['check']
+            expect = case_data['validate'][0]['expect']
+            apicheck = apiResponseData[check]
+            print(expect)
+            #print(apicheck)
+            if (comparator == 'eq'):
+                pass
         except Exception as e:
             logger.exception('测试用例对比失败，{}'.format(e))
         pass
 
 
-handle_check_Result = HandleCheckResult()
+# handle_check_Result = HandleCheckResult()
+
+if __name__ == '__main__':
+    handle_check_Result = HandleCheckResult()
+    apiResponseData = {"result": 0,
+                       "data": [{"word": "testng"}, {"word": "test"}, {"word": "testlink"}, {"word": "testn"},
+                                {"word": "testng教程"}], "msg": "成功"}
+    case_data = {"name": "测试用例1", "params": {"words": "Test"},
+                 "validate": [{"check": "msg", "comparator": "eq", "expect": "成功"}]}
+    handle_check_Result.check_result(apiResponseData, case_data)
