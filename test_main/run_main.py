84c8034660ccf688b578341f1747ac50a759e575
ww <ww23928@ly.com>
2020-08-11 17:26:35 +0800

    allure

--- test_main/run_main.py
+++ test_main/run_main.py
@@ -9,4 +9,4 @@ curPath = os.path.abspath(os.path.dirname(__file__))
 BasePath = curPath[:curPath.find("Apiautomation\\") + len("Apiautomation\\")]
 
 if __name__ == "__main__":
-    pytest.main(['-s', '-v', 'test_case/testRequest/', '-q', '--alluredir', 'reports/result'])
+    pytest.main(['-s', '-v', 'test_case/testRequest/', '-q', '--alluredir', 'reports'])
