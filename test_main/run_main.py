0a39391f730beb2fc4aaad4a71911c07f7bd07d2
ww <ww23928@ly.com>
2020-08-11 17:43:22 +0800

    1111

--- test_main/run_main.py
+++ test_main/run_main.py
@@ -4,9 +4,8 @@ import sys
 import os
 
 sys.path.append('../')
-sys.path.append('../Apiautomation')
 curPath = os.path.abspath(os.path.dirname(__file__))
 BasePath = curPath[:curPath.find("Apiautomation\\") + len("Apiautomation\\")]
 
 if __name__ == "__main__":
-    pytest.main(['-s', '-v', 'test_case/testRequest/', '-q', '--alluredir', 'reports'])
+    pytest.main(['-s', '-v', 'test_case/testRequest/', '-q', '--alluredir', 'Apiautomation/reports'])
